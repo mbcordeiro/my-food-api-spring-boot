0a4c353b6ea540e66d741e0b7411431db9fe73a6
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-04-22 23:58:58 -0300

    Swagger congif

---
+++ src/main/java/com/matheuscordeiro/myfoodapi/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.matheuscordeiro.myfoodapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("My food api")
+                .description("Api project ecommerce , delivery and payment of food from restaurants ")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact("Matheus Cordeiro", "http://github.com/matheusBCordeiro", "matheusdebarroscordeiro@gmail.com");
+    }
+}
+
