6f18aa86a0b1f4386b3050b727c4ff80bbb319b6
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-05-06 21:58:05 -0300

    Refactor swagger config

--- src/main/java/com/matheuscordeiro/myfoodapi/config/SwaggerConfig.java
+++ src/main/java/com/matheuscordeiro/myfoodapi/config/SwaggerConfig.java
@@ -1,10 +1,7 @@
 package com.matheuscordeiro.myfoodapi.config;
 
-import io.github.jhipster.config.JHipsterConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,8 +11,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Primary
-@Profile(JHipsterConstants.SPRING_PROFILE_SWAGGER)
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
